58f4d9040e07efee5a2726f2cfc1898efaa1356c
iwjd0 <130964631+iwjd0@users.noreply.github.com>
2023-04-18 10:35:38 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -36,11 +36,13 @@ app.post('/', async (req, res) => {
 //)
 
 
-completion = openai.ChatCompletion.create(
+completion = openai.ChatCompletion.create({
   model="gpt-3.5-turbo",
   messages=[
     {"role": "user", "content": "Hello!"}
   ]
+  
+  }
 )
   
 
